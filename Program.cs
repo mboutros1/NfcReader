fedac2ae043d5de4e65aafbf6ab6c7e57e05d80a
Mike B <miboutros@deloitte.com>
2019-12-03 09:54:17 -0500

    exe

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ namespace Reading
         {
             Console.WriteLine("Reading NFC Readers");
             var nfc = new NFCReader();
+            try
+            {
+                nfc.Connect();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed to connect");
+                Console.WriteLine(e);
+            }
             var readers = nfc.GetReadersList();
             Console.WriteLine("Found " + readers.Count);
             foreach (var reader in readers)

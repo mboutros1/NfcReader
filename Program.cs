86400e0f5bb9a391c9dd0ef71134a0e7bf8aeb04
Mike B <miboutros@deloitte.com>
2019-12-03 09:21:58 -0500

    list readers

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿ 
+using System;
+
+namespace Reading
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Reading NFC Readers");
+            var nfc = new NFCReader();
+            var readers = nfc.GetReadersList();
+            Console.WriteLine("Found " + readers.Count);
+            foreach (var reader in readers)
+            {
+                Console.WriteLine(reader);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
